4b0748838b86c76b5bc8052d66f4dbfcf1ef3452
Tyler Britten <tyler.britten@hpe.com>
2023-05-23 09:26:50 -0400

    test preprocess brain

--- brain-mri/container/deploy/brain_mri_handler.py
+++ brain-mri/container/deploy/brain_mri_handler.py
@@ -36,7 +36,7 @@ class BrainHandler(ImageClassifier):
         Returns:
             tensor: A torch tensor in correct format for brain mri unet model
         """
-        
+        print(data)
         tensor_data = data[0]["data"]
         tensor_shape = data[0]["shape"]
         output = torch.FloatTensor(np.array(tensor_data).reshape(tensor_shape))
